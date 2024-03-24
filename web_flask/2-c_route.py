ba380f3479d521960bac0b9d4fc8da9b41fb1da8
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-03-24 00:22:54 -0700

    C is the best :::::git add .git add .git add .

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,5 +13,17 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ return HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
