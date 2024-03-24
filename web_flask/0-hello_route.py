f6c1f71fea05d81a0bb9c1b9984455f41f0a962c
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-03-24 00:01:37 -0700

    first flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+start Flask application
+"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -6,8 +9,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
